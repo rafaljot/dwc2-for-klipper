0e8f1218933ffee85e034d31f2428ce0488a7ee6
rafaljot <rafal@jastrzebscy.org>
2019-04-28 23:28:24 +0200

    Update web_dwc2.py
    Add support for special characters in file names like "łeb żółtego łabędzia.gcode"

--- web_dwc2.py
+++ web_dwc2.py
@@ -19,6 +19,7 @@ import util
 import shutil
 import serial
 
+
 class web_dwc2:
 
 ##
@@ -31,7 +32,7 @@ class web_dwc2:
 		self.last_state = 'O'
 		self.popup = None
 		self.message = None
-		self.serial = serial.Serial('/tmp/printer', 250000, timeout=0.050)
+		self.serial = serial.Serial('/tmp/printer', 250000, timeout=0.250)
 		#	get config
 		self.config = config
 		self.adress = config.get( 'listen_adress', "127.0.0.1" )
@@ -283,6 +284,9 @@ class web_dwc2:
 			except Exception as e:
 				logging.warn( "DWC2 - error in write: " + str(e) )
 
+		def decode_argument(self, value, name=None):
+			return value.encode('UTF-8') if isinstance(value, unicode) else value
+
 ##
 #	All the crazy shit dc42 fiddled together in c
 ##
@@ -444,7 +448,7 @@ class web_dwc2:
 
 		#	append elements to files list matching rrf syntax
 		for el_ in os.listdir(path_):
-			el_path = path_ + "/" + el_
+			el_path = path_ + "/" + str(el_)
 			repl_['files'].append({
 				"type": "d" if os.path.isdir(el_path) else "f" ,
 				"name": str(el_) ,
